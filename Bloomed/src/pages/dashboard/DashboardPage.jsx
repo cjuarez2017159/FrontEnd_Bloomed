8bea11a190a201f3e7a0c4f18c244302d6235ecb
darriola2021670 <darriola-2021670@kinal.edu.gt>
2024-06-12 17:19:14 -0600

    dashboard hecho

---
+++ Bloomed/src/pages/dashboard/DashboardPage.jsx
@@ -0,0 +1,20 @@
+import { Navbar, Container, Nav } from "react-bootstrap";
+
+function CustomNavbar() {
+  return (
+    <Navbar expand="lg" bg="dark" variant="dark" fixed="top">
+      <Container>
+        <Navbar.Brand href="#home">Mi Blog</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Inicio</Nav.Link>
+            <Nav.Link href="#link">Enlace</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default CustomNavbar;
