339ea575971267e06e49c7aabe43f92e6dbea652
darriola2021670 <darriola-2021670@kinal.edu.gt>
2024-07-08 17:18:17 -0600

    base de la pagina principal falta ponerle diseño

--- Bloomed/src/main.jsx
+++ Bloomed/src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 //import App from './App.jsx'
-import './index.css'
+//import './index.css'
+import "./components/Home.css"
 import { Home } from './components/Home.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
